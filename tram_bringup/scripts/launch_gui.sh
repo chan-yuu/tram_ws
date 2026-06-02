78ff65ee12761c0cf69c2ec7d2f9f1bf5baac1ba
cyun <129357834+chan-yuu@users.noreply.github.com>
2026-06-02 22:35:41 +0800

    Delete tram_bringup/scripts/launch_gui.sh

--- tram_bringup/scripts/launch_gui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DISABLE_ROS1_EOL_WARNINGS=1
-source /home/cyun/Documents/project/tram_ws/devel/setup.bash
-python3 /home/cyun/Documents/project/tram_ws/src/tram_bringup/scripts/scenario_gui.py
